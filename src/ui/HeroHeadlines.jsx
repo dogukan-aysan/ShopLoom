9f9920a1030ebda4e500a93a1adf53ad864bb0a4
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-12 14:09:31 +0300

    add new font family for brand

--- src/ui/HeroHeadlines.jsx
+++ src/ui/HeroHeadlines.jsx
@@ -1,10 +1,11 @@
 function HeroHeadlines() {
   return (
     <div className="hero__headlines">
-      <h1 className="hero__headlines--main">
-        Navigate the World of Products Effortlessly with <span>ShopLoom</span>
+      <h1 className="hero__main">
+        Navigate the World of Products Effortlessly with
       </h1>
-      <h2 className="hero__headlines--secondary">
+      <span className="hero__brand">ShopLoom</span>
+      <h2 className="hero__secondary">
         Explore, Click, Enjoy: We Put the Joy Back in Shopping.
       </h2>
     </div>
