de92bc1f162a5501fb5a32df336b5addef31c5c7
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-12 10:52:26 +0300

    make shopping-cart icon responsive

--- src/ui/HiddenLinks.jsx
+++ src/ui/HiddenLinks.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import HeaderAuth from "./HeaderAuth";
 
 function HiddenLinks() {
+  const navigate = useNavigate();
   return (
     <ul className="header__hidden-links">
       <li>
@@ -8,6 +10,9 @@ function HiddenLinks() {
       </li>
       <li>
         <span className="hidden-links--contact">Contact</span>
+      </li>
+      <li onClick={() => navigate("/app/shopping-cart")}>
+        <span className="hidden-links--cart">Shopping Cart</span>
       </li>
       <li>
         <div className="hidden-links--auth">
